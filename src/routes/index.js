4bae0d1658401641f0475ebcdae13709576be018
paulp88 <poterasi_paul@yahoo.com>
2016-09-02 01:19:27 +0300

    add src
    add src

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var games = require('../services/games');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var visits = parseInt(req.cookies.visits) || 0;
+  var name = req.cookies.NameP || "Paul";
+  var userId=req.agent.id;
+  
+  visits += 1;
+  res.cookie('visits', visits);
+  res.cookie('NameP', name);
+  res.render('index',
+            { title: 'Hangman', 
+              name: 'World', 
+              visits: visits,
+              myName: name,
+              userIdN: userId,
+              createdGames: games.createdBy(userId),
+              availableGames: games.availableTo(userId),
+              partials: { createdGame: 'createdGame' }
+            }
+            );
+});
+
+module.exports = router;
